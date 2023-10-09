0b7985333001be4547875d036b0f7e4982fb19bb
MackendyTechnology <bouquetmackendy34@gmail.com>
2023-10-09 18:17:12 -0400

    Exercice-JS-3

---
+++ index.js
@@ -0,0 +1,29 @@
+function areEqual(x, y) {
+    return x === y;
+ }
+ console.log(areEqual(5, 5));
+ 
+ function HaveTheSameLength(str1, str2) {
+    return str1.length === str2.length;
+ }
+ console.log(HaveTheSameLength('hello', 'world'));
+ 
+ function lessThanNinety(num) {
+    return num < 90;
+ }
+ console.log(lessThanNinety(80));
+ 
+ function greaterthanfifty(num) {
+    return num > 50;
+ }
+ console.log(greaterthanfifty(60));
+ 
+ function isEven(num) {
+    return num % 2 === 0;
+ }
+ console.log(isEven(4));
+ 
+ function isOdd(num) {
+    return num % 2 !== 0;
+ }
+ console.log(isOdd(3));
